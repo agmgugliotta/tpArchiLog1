aa28755ffce83a89ead61e8797ef08ab3cc08fda
Anthony <agm.gugliotta@gmail.com>
2020-06-11 08:33:13 +0200

    Day 3, basemodel debug

--- src/Core/ApiLibrary/Core/Entity/BaseModel.cs
+++ src/Core/ApiLibrary/Core/Entity/BaseModel.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace ApiLibrary.Core.Entity
 {
-    class BaseModel<T> : BaseEntity
+    public class BaseModel<T> : BaseEntity
     {
         public T ID { get; set; }
     }
